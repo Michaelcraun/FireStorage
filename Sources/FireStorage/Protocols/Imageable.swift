723afe9715541bef93f837e1996925eac8c631cf
Michael Craun <michael.craun@gmail.com>
2022-09-28 09:14:10 -0400

    Added Imageable protocol
    This will allow the developer to associate, fetch, and store image data to their Firebase project for any data type

---
+++ Sources/FireStorage/Protocols/Imageable.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael Craun on 9/28/22.
+//
+
+import Foundation
+
+public protocol Imageable: AnyObject {
+    typealias ImageGetCompletion = (Data?, Error?) -> Void
+    typealias ImagePutCompletion = (String?, Error?) -> Void
+    
+    var imageData: Data? { get set }
+    
+    func image(name: String, extension: String, completion: ImageGetCompletion?)
+    func putImage(data: Data, name: String, extension: String, completion: ImagePutCompletion?)
+}
